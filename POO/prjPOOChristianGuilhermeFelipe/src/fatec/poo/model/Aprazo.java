094bd5295560f9acf7afc2c1a5b46481b4661fb2
CHRISTIAN RODRIGO CORRALES <christian.corrales@fatec.sp.gov.br>
2018-11-02 11:35:52 -0300

    Initial files

---
+++ POO/prjPOOChristianGuilhermeFelipe/src/fatec/poo/model/Aprazo.java
@@ -0,0 +1,34 @@
+package fatec.poo.model;
+
+public class APrazo {
+    private double valor;
+    private String dtVencimento;
+    private double taxaJuros;
+    private int qtdeMensalidade;
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    public void setDtVencimento(String dtVencimento) {
+        this.dtVencimento = dtVencimento;
+    }
+    public void setTaxajuros(double taxaJuros) {
+        this.taxaJuros = taxaJuros;
+    }
+    public void setQtdeMensalidade(int qtdeMensalidade) {
+        this.qtdeMensalidade = qtdeMensalidade;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+    public String getDtVencimento() {
+        return dtVencimento;
+    }
+    public double getTaxajuros() {
+        return taxaJuros;
+    }
+    public int getQtdeMensalidade() {
+        return qtdeMensalidade;
+    }
+}
